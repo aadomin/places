2dbe32c855c779e44996a867db0135ce0a5f7a5a
aadomin <aadomin@yandex.ru>
2021-04-01 15:28:58 +0300

    widget Text

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -9,44 +9,36 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(130.0), // here the desired height
-        child: AppBar(
-          title: SizedBox(
-            width: 300,
-            height: 100,
+      appBar: AppBar(
+        toolbarHeight: 140,
+        title: Padding(
+          padding: EdgeInsets.only(
+            top: 60,
+          ),
+          child: SizedBox(
+            width: 250,
+            height: 80,
             child: Text(
               'Список интересных мест',
               maxLines: 2,
               style: TextStyle(
-                fontSize: 35,
+                fontSize: 30,
                 fontWeight: FontWeight.bold,
-                color: Colors.black,
+                color: Colors.grey.shade700,
               ),
             ),
           ),
-          backgroundColor: Colors.white,
-          elevation: 0,
-          toolbarHeight: 50,
         ),
+        backgroundColor: Colors.white,
+        elevation: 0,
       ),
       body: Center(
         child: ListView.builder(
-          itemCount: 77,
+          itemCount: 100,
           itemBuilder: (context, i) {
-            return Text('Lorem ispirum');
+            return Text('$i. Lorem ipsum');
           },
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Дом'),
-          BottomNavigationBarItem(icon: Icon(Icons.work), label: 'Работа'),
-          BottomNavigationBarItem(icon: Icon(Icons.battery_full), label: 'Бар'),
-        ],
-        backgroundColor: Colors.blue.shade200,
-        fixedColor: Colors.pink.shade200,
-        unselectedItemColor: Colors.grey.shade300,
       ),
     );
   }
