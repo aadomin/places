f8886efda8f52f90ec3b18f5fe0af5f52c359586
Andrew Domin <aadomin@yandex.ru>
2021-09-07 12:12:40 +0300

    renaming done, remove some models, core

--- lib/ui/screens/add_sight_screen/add_sight_model.dart
+++ lib/ui/screens/add_sight_screen/add_sight_model.dart
@@ -1,19 +1,20 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:places/data/interactors/place_interactor.dart';
 
 import 'package:places/data/repositories/place_repository.dart';
-import 'package:places/data/interactors/_core.dart';
 import 'package:places/data/models/place.dart';
 
 class AddSightModel with ChangeNotifier {
-  var core = CoreSingleton();
+  final placeInteractor = PlaceInteractor();
 
   List<String>? _listOfPhotos;
 
   List<String> get listOfPhotos {
     // копируем изначальный список фоток, впоследствии он будет удален
+
     _listOfPhotos ??= [
-      ...core.sightsStorage.listOfInitialPhotosForAdding,
+      ...placeInteractor.listOfInitialPhotosForAdding,
     ];
 
     return _listOfPhotos ?? []; // useless thing just for null safety
