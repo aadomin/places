ac7a0f5f77769d152f01bd47ea55926043e4ad40
Andrew Domin <aadomin@yandex.ru>
2021-09-30 15:02:00 +0300

    di

---
+++ lib/di.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:places/ui/my_app/my_app.dart';
+
+import 'package:places/data/interactors/place_interactor.dart';
+import 'package:places/data/interactors/settings_interactor.dart';
+import 'package:places/data/interactors/filter_interactor.dart';
+import 'package:places/ui/screens/select_category_screen/select_category_screen_model.dart';
+import 'package:places/data/interactors/search_interactor.dart';
+
+class DI extends StatelessWidget {
+  const DI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => SettingsInteractor(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => FilterInteractor(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => PlaceInteractor(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => SearchInteractor(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => SelectCategoryScreenModel(),
+        ),
+      ],
+      child: const MyApp(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import 'package:places/ui/my_app/my_app.dart';
-
-import 'package:places/data/interactors/place_interactor.dart';
-import 'package:places/data/interactors/settings_interactor.dart';
-import 'package:places/data/interactors/filter_interactor.dart';
-import 'package:places/ui/screens/select_category_screen/select_category_screen_model.dart';
-import 'package:places/data/interactors/search_interactor.dart';
+import 'package:places/di.dart';
 
 void main() {
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => SettingsInteractor(),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => FilterInteractor(),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => PlaceInteractor(),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => SearchInteractor(),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => SelectCategoryScreenModel(),
-        ),
-      ],
-      child: const MyApp(),
-    ),
+    const DI(),
   );
 }
