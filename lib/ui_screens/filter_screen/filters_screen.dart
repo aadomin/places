4bdf451b662b5acbd98540177f8fd9a17c765a29
Andrew Domin <aadomin@yandex.ru>
2022-01-25 12:01:16 +0300

    try to fix overflow

--- lib/ui_screens/filter_screen/filters_screen.dart
+++ lib/ui_screens/filter_screen/filters_screen.dart
@@ -67,139 +67,146 @@ class _FiltersScreenState extends State<FiltersScreen> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          //
-          // Название раздела "КАТЕГОРИИ"
-          //
-          Align(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(16, 24, 0, 24),
-              child: Text(
-                UiStrings.categories,
-                style: TextStyle(
-                  color: Theme.of(context).primaryColorLight,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //
+            // Название раздела "КАТЕГОРИИ"
+            //
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16, 24, 0, 24),
+                child: Text(
+                  UiStrings.categories,
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColorLight,
+                  ),
                 ),
               ),
             ),
-          ),
 
-          //
-          // Сам список категорий
-          //
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Wrap(
-              runSpacing: 20,
-              spacing: 20,
-              children: [
-                for (var i = 0;
-                    i < context.watch<FilterInteractor>().filterItems.length;
-                    i++)
-                  SizedBox(
-                    width: 110,
-                    height: 95,
-                    child: TextButton(
-                      onPressed: () {
-                        context
-                            .read<FilterInteractor>()
-                            .switchActiveCategories(i);
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 10.0),
-                        child: Column(
-                          children: [
-                            Stack(
-                              children: [
-                                ClipRRect(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(_radiusOfRoundElement),
-                                  ),
-                                  child: Container(
-                                    color: const Color(0x4fbAbAbF),
-                                    width: _radiusOfRoundElement * 2,
-                                    height: _radiusOfRoundElement * 2,
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Center(
-                                        child: Image.asset(
-                                          context
-                                              .watch<FilterInteractor>()
-                                              .filterItems[i]
-                                              .assetPath,
-                                          width: 32,
-                                          height: 32,
+            //
+            // Сам список значков категорий
+            //
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Wrap(
+                runSpacing: 20,
+                spacing: 20,
+                children: [
+                  for (var i = 0;
+                      i < context.watch<FilterInteractor>().filterItems.length;
+                      i++)
+                    SizedBox(
+                      width: 110,
+                      height: 105,
+                      child: TextButton(
+                        onPressed: () {
+                          context
+                              .read<FilterInteractor>()
+                              .switchActiveCategories(i);
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 10.0),
+                          child: Column(
+                            children: [
+                              Stack(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(_radiusOfRoundElement),
+                                    ),
+                                    child: Container(
+                                      color: const Color(0x4fbAbAbF),
+                                      width: _radiusOfRoundElement * 2,
+                                      height: _radiusOfRoundElement * 2,
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Center(
+                                          child: Image.asset(
+                                            context
+                                                .watch<FilterInteractor>()
+                                                .filterItems[i]
+                                                .assetPath,
+                                            width: 32,
+                                            height: 32,
+                                          ),
                                         ),
                                       ),
                                     ),
                                   ),
-                                ),
-                                //ignore: prefer_if_elements_to_conditional_expressions
-                                context
-                                        .watch<FilterInteractor>()
-                                        .filterItems[i]
-                                        .isSelected
-                                    ? WidgetCheckmark(
-                                        radiusOfRoundElement:
-                                            _radiusOfRoundElement,
-                                      )
-                                    : const SizedBox.shrink(),
-                              ],
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(4, 12, 4, 0),
-                              child: Text(
-                                context
-                                    .watch<FilterInteractor>()
-                                    .filterItems[i]
-                                    .name,
-                                style: const TextStyle(
-                                  fontSize: 13,
+                                  //ignore: prefer_if_elements_to_conditional_expressions
+                                  context
+                                          .watch<FilterInteractor>()
+                                          .filterItems[i]
+                                          .isSelected
+                                      ? WidgetCheckmark(
+                                          radiusOfRoundElement:
+                                              _radiusOfRoundElement,
+                                        )
+                                      : const SizedBox.shrink(),
+                                ],
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(4, 12, 4, 0),
+                                child: Text(
+                                  context
+                                      .watch<FilterInteractor>()
+                                      .filterItems[i]
+                                      .name,
+                                  style: const TextStyle(
+                                    fontSize: 12,
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-              ],
-            ),
-          ),
-
-          //
-          // Слово "Расстояние"
-          //
-          ListTile(
-            title: Text(
-              'Расстояние',
-              style: TextStyle(
-                color: Theme.of(context).primaryColor,
+                ],
               ),
             ),
-            trailing: Text(
-              _distanceList[(_sliderValue * 10).toInt()].toString(),
-              style: TextStyle(
-                color: Theme.of(context).primaryColorLight,
-              ),
-            ),
-            onTap: () {},
-          ),
 
-          //
-          // Слайдер
-          //
-          Slider(
-            divisions: 10,
-            value: _sliderValue,
-            onChanged: (newValue) {
-              setState(() {
-                _sliderValue = newValue;
-              });
-            },
-          ),
-        ],
+            //
+            // Слово "Расстояние"
+            //
+            ListTile(
+              title: Text(
+                'Расстояние',
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+              trailing: Text(
+                _distanceList[(_sliderValue * 10).toInt()].toString(),
+                style: TextStyle(
+                  color: Theme.of(context).primaryColorLight,
+                ),
+              ),
+              onTap: () {},
+            ),
+
+            //
+            // Слайдер
+            //
+            Slider(
+              divisions: 10,
+              value: _sliderValue,
+              onChanged: (newValue) {
+                setState(() {
+                  _sliderValue = newValue;
+                });
+              },
+            ),
+
+            //
+            // Отступ
+            //
+            const SizedBox(height: 70),
+          ],
+        ),
       ),
       bottomSheet: Container(
         //
