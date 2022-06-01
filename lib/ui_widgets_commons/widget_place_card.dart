0e5e9cc5616dcdecdfbf982f6c0836ac87197e75
Andrew Domin <aadomin@yandex.ru>
2022-06-01 16:33:50 +0300

    fix dismissible on main screen

--- lib/ui_widgets_commons/widget_place_card.dart
+++ lib/ui_widgets_commons/widget_place_card.dart
@@ -78,152 +78,162 @@ class WidgetPlaceCard extends StatelessWidget {
           color: Theme.of(context).disabledColor,
           child: InkWell(
             onTap: onTap,
-            child: Dismissible(
-              key: ValueKey(place.id),
-              onDismissed: onDismissedAction,
-              background: Container(
-                alignment: Alignment.centerLeft,
-                color: Colors.red,
-                child: Padding(
-                  padding: const EdgeInsets.all(24.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.restore_from_trash,
-                          color: Theme.of(context).canvasColor),
-                      Text(
-                        textWhenDeletion,
-                        style: TextStyle(
-                          color: Theme.of(context).canvasColor,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              secondaryBackground: Container(
-                alignment: Alignment.centerRight,
-                color: Colors.red,
-                child: Padding(
-                  padding: const EdgeInsets.all(24.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.restore_from_trash,
-                        color: Theme.of(context).canvasColor,
-                      ),
-                      Text(
-                        textWhenDeletion,
-                        style: TextStyle(
-                          color: Theme.of(context).canvasColor,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              //
-              // Сама карточка
-              //
-              child: Container(
-                padding: const EdgeInsets.only(bottom: 16),
-                child: Stack(
-                  children: [
-                    Column(
-                      children: [
-                        //
-                        // Картинка
-                        //
-                        SizedBox(
-                          width: double.infinity,
-                          height: 100,
-                          child: ClipRRect(
-                            borderRadius: const BorderRadius.only(
-                              topLeft: Radius.circular(12),
-                              topRight: Radius.circular(12),
-                            ),
-                            child: WidgetMyImageInk(
-                              url: place.url.isNotEmpty ? place.url[0] : '',
-                              // СДЕЛАТЬ: не помню
-                              fit: BoxFit.fitWidth,
-                            ),
-                          ),
-                        ),
-                        //
-                        // Название места
-                        //
-                        Container(
-                          padding: const EdgeInsets.only(
-                              top: 16, right: 16, left: 16),
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            place.name,
-                            maxLines: 1,
-                            style: TextStyle(
-                              fontWeight: FontWeight.w700,
-                              fontSize: 16,
-                              color: Theme.of(context).primaryColor,
-                            ),
-                          ),
-                        ),
-                        //
-                        // Детальное описание
-                        //
-                        Container(
-                          padding: const EdgeInsets.only(
-                              top: 4, right: 16, left: 16),
-                          alignment: Alignment.topLeft,
-                          child: ConstrainedBox(
-                            constraints: const BoxConstraints(),
-                            child: Text(
-                              place.details,
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 4,
+            child: (placeCardType == PlaceCardType.general)
+                // Карточка
+                ? widgetCardItself(context)
+
+                // Смещабельная карточка
+                : Dismissible(
+                    key: ValueKey(place.id),
+                    onDismissed: onDismissedAction,
+                    background: Container(
+                      alignment: Alignment.centerLeft,
+                      color: Colors.red,
+                      child: Padding(
+                        padding: const EdgeInsets.all(24.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.restore_from_trash,
+                                color: Theme.of(context).canvasColor),
+                            Text(
+                              textWhenDeletion,
                               style: TextStyle(
-                                color: Theme.of(context).primaryColorLight,
+                                color: Theme.of(context).canvasColor,
                               ),
                             ),
-                          ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
-                    //
-                    // Type of place on the top of card
-                    //
-                    Container(
-                      padding: const EdgeInsets.all(16),
-                      width: double.infinity,
-                      child: Text(
-                        place.type.toLowerCase(),
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w700,
+                    secondaryBackground: Container(
+                      alignment: Alignment.centerRight,
+                      color: Colors.red,
+                      child: Padding(
+                        padding: const EdgeInsets.all(24.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.restore_from_trash,
+                              color: Theme.of(context).canvasColor,
+                            ),
+                            Text(
+                              textWhenDeletion,
+                              style: TextStyle(
+                                color: Theme.of(context).canvasColor,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
                     //
-                    // Icons on the top of card
+                    // Сама карточка
                     //
-                    Container(
-                      alignment: Alignment.topRight,
-                      padding: const EdgeInsets.all(8),
-                      child: WidgetPlaceCardIcons(
-                        placeCardType: placeCardType,
-                        onDeleteFromSeen: onDeleteFromSeen,
-                        onDeleteFromWished: onDeleteFromWished,
-                        onAddToCalendar: onAddToCalendar,
-                        onToggleWished: onToggleWished,
-                        onShare: onShare,
-                        isLiked: isLiked,
-                      ),
-                    ),
-                  ],
+                    child: widgetCardItself(context),
+                  ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  //
+  // Сама карточка
+  //
+  Widget widgetCardItself(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(bottom: 16),
+      child: Stack(
+        children: [
+          Column(
+            children: [
+              //
+              // Картинка
+              //
+              SizedBox(
+                width: double.infinity,
+                height: 100,
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(12),
+                    topRight: Radius.circular(12),
+                  ),
+                  child: WidgetMyImageInk(
+                    url: place.url.isNotEmpty ? place.url[0] : '',
+                    // СДЕЛАТЬ: не помню
+                    fit: BoxFit.fitWidth,
+                  ),
                 ),
+              ),
+              //
+              // Название места
+              //
+              Container(
+                padding: const EdgeInsets.only(top: 16, right: 16, left: 16),
+                alignment: Alignment.topLeft,
+                child: Text(
+                  place.name,
+                  maxLines: 1,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+              ),
+              //
+              // Детальное описание
+              //
+              Container(
+                padding: const EdgeInsets.only(top: 4, right: 16, left: 16),
+                alignment: Alignment.topLeft,
+                child: ConstrainedBox(
+                  constraints: const BoxConstraints(),
+                  child: Text(
+                    place.details,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 4,
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColorLight,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          //
+          // Type of place on the top of card
+          //
+          Container(
+            padding: const EdgeInsets.all(16),
+            width: double.infinity,
+            child: Text(
+              place.type.toLowerCase(),
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
               ),
             ),
           ),
-        ),
+          //
+          // Icons on the top of card
+          //
+          Container(
+            alignment: Alignment.topRight,
+            padding: const EdgeInsets.all(8),
+            child: WidgetPlaceCardIcons(
+              placeCardType: placeCardType,
+              onDeleteFromSeen: onDeleteFromSeen,
+              onDeleteFromWished: onDeleteFromWished,
+              onAddToCalendar: onAddToCalendar,
+              onToggleWished: onToggleWished,
+              onShare: onShare,
+              isLiked: isLiked,
+            ),
+          ),
+        ],
       ),
     );
   }
